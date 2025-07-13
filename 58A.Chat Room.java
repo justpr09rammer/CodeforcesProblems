73522df4de2665372184840b0404b6f00231e273
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2025-07-13 13:25:56 +0400

    Create 58A.Chat Room.java

---
+++ 58A.Chat Room.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String str = "hello";
+
+        String s = scanner.nextLine();
+        int leftPointerStr = 0;
+        int leftPointerS = 0;
+        while (leftPointerS < s.length()){
+            if (leftPointerStr == 5){
+                System.out.println("YES");
+                return;
+            }
+            if (str.charAt(leftPointerStr) == s.charAt(leftPointerS)){
+                leftPointerStr ++;
+            }
+            leftPointerS ++;
+        }
+        System.out.println("NO");
+    }
+
+}
