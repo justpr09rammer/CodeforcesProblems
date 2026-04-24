2f0b3a0b8d8d09927cb0c7c1862dd39f393f8a77
nihad.001 <163444756+justpr09rammer@users.noreply.github.com>
2026-04-24 23:29:39 +0400

    solution to 279B in codeforces

---
+++ 279_B_Books.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+#include <vector>
+using namespace std;
+
+
+int main(){
+	
+	int n, t;
+	cin >> n >> t;
+	vector<long long> v1(n);
+	for (int i = 0; i < n; i ++){
+		cin >> v1[i];
+	}
+	long long left_pointer = 0;
+	long long right_pointer = -1;
+	long long result = 0;
+	long long sum = 0;
+	while (left_pointer < n && right_pointer < n){
+		while (sum <= t && right_pointer < n){
+			right_pointer ++;
+			sum += v1[right_pointer];
+		}
+		result = max(result, right_pointer - left_pointer);
+		sum -= v1[left_pointer];
+		left_pointer ++;
+	}
+	cout << result;
+}
